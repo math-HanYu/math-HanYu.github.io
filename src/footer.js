39fdb97f52e2c1f23852183a73847c721824c8b5
math-HanYu <122560196+math-HanYu@users.noreply.github.com>
2026-06-13 19:47:56 +0800

    Update footer.js

--- src/footer.js
+++ src/footer.js
@@ -4,7 +4,7 @@ const firstName = "Han";
 const familyName = "Yu";
 const email = `${firstName}.${familyName}.2@cqu.edu.cn`;
 
-const Room = "理科楼LD301";
+const Room = "理科楼LD301(B)";
 const address = "College of Mathematics and Statistics, Center of Mathematics, Chongqing University, Chongqing, 401331, China";
 
 
